b9c616c5da7ad3ed736204eb60e3f4aececebdfc
Jakob Blomer <jblomer@cern.ch>
2014-07-11 01:37:12 -0700

    evening

--- histogram.cc
+++ histogram.cc
@@ -4,9 +4,21 @@
 #include <cstdio>
 
 int main() {
-  Histogram<float> h;
+  Histogram<float, uint32_t> h;
+  h.SetDimensions(1);
+  std::vector<float> bins;
+  for (unsigned i = 0; i <= 10000; ++i) {
+    bins.push_back(i);
+  }
+  //h.SetBinning(1, Histogram<float, uint32_t>::Binning(bins));
+  h.SetBinning(1, Histogram<float, uint32_t>::Binning(0, 1, 10000));
+  for (unsigned j = 0; j < 10000; ++j) {
+    for (unsigned i = 0; i < 10000; ++i) {
+      h.Fill(i);
+    }
+  }
 
-  Histogram<float>::BinningFixed b1(0.0, 1.0, 10.0);
+  /*Histogram<float>::BinningFixed b1(0.0, 1.0, 10.0);
   printf("0.0: %d\n", b1.FindBin(0.0));
   printf("-0.1: %d\n", b1.FindBin(-0.1));
   printf("11.0: %d\n", b1.FindBin(11.0));
@@ -39,7 +51,7 @@ int main() {
   printf("3.5: %d\n", b4.FindBin(3.5));
   printf("9999: %d\n", b4.FindBin(9999));
   printf("10000: %d\n", b4.FindBin(10000));
-  printf("10001: %d\n", b4.FindBin(10001));
+  printf("10001: %d\n", b4.FindBin(10001));*/
 
   return 0;
 }
--- histogram.h
+++ histogram.h
@@ -6,85 +6,98 @@
 #include <math.h>
 #include <vector>
 
-template <typename Floating>
+template <typename BorderType, typename ChannelType>
 class Histogram {
  public:
-  template<class Derived>
   class Binning {
    public:
-     uint32_t FindBin(Floating val) const {
-       return static_cast<const Derived *>(this)->FindBinImpl(val);
+     Binning() {
+
      }
-     uint32_t GetMax() const {
-       return static_cast<const Derived *>(this)->GetMaxImpl();
+     Binning(BorderType min, BorderType width, BorderType max)
+       : min_(min)
+       , max_(max)
+       , width_(width)
+       , max_bin_(ceil((max-min)/width) + 1)
+     { }
+     Binning(const std::vector<BorderType> &borders)
+       : borders_(borders)
+       , min_(borders[0])
+       , max_(borders.back())
+       , width_(0)
+       , max_bin_(borders_.size())
+     { }
+
+     uint32_t FindBin(BorderType val) const {
+       if (val < min_) return 0;
+       if (val >= max_) return max_bin_;
+       if (width_ > 0) return (val-min_)/width_ + 1;
+
+       uint32_t lo = 0;
+       uint32_t hi = max_bin_;
+       while (hi-lo > kSteps) {
+         uint32_t skip = (hi-lo)/kSteps;
+         unsigned i = lo + skip;
+         for ( ; i < hi; i += skip)
+           if (borders_[i] >= val) break;
+         hi = (i > hi) ? hi : i;
+         lo = i - skip;
+       }
+       unsigned i = lo;
+       for ( ; i < hi; ++i)
+         if (borders_[i] > val) break;
+       return i;
      }
-   protected:
-    uint32_t FindBinImpl(Floating val) const { return uint32_t(-1); };
-    uint32_t GetMaxImpl() const { return 0; };
+
+     uint32_t max_bin() const { return max_bin_; }
+   private:
+    static const uint8_t kSteps = 16;
+
+    std::vector<BorderType> borders_;
+    BorderType min_;
+    BorderType max_;
+    BorderType width_;
+    uint32_t max_bin_;
   };
 
-  class BinningFixed : public Binning<BinningFixed> {
-    friend class Binning<BinningFixed>;
-   public:
-    BinningFixed(Floating min, Floating width, Floating max)
-      : min_(min)
-      , max_(max)
-      , width_(width)
-      , max_bin_(ceil((max-min)/width) + 1)
-    { }
+  Histogram()
+    : dimensions_(0)
+    , binnings_(nullptr)
+    , max_bins_(nullptr)
+    , bins_(nullptr)
+  { }
+  ~Histogram() {
+    //Clear();
+  }
+  void SetDimensions(const uint8_t dimensions) {
+    Clear();
+    dimensions_ = dimensions;
+    binnings_ = new Binning[dimensions_];
+    max_bins_ = new uint32_t[dimensions_];
 
-   protected:
-    uint32_t FindBinImpl(Floating val) const {
-      if (val < min_) return 0;
-      if (val >= max_) return max_bin_;
-      return (val-min_)/width_ + 1;
-    }
-    uint32_t GetMax() const { return max_bin_; }
+    //TODO
+    bins_ = new ChannelType[max_bins_[dimensions_-1]];
+  }
+  void SetBinning(const uint8_t dimension, const Binning &binning) {
+    max_bins_[dimension-1] = binning.max_bin();
+    binnings_[dimension-1] = binning;
+  }
+  void Fill(BorderType val) {
+    uint32_t bin_id = binnings_[0].FindBin(val);
+    bins_[bin_id]++;
+  }
 
-    const Floating min_;
-    const Floating max_;
-    const Floating width_;
-    const uint32_t max_bin_;
-  };
+ private:
+  void Clear() {
+    delete[] binnings_;
+    delete[] max_bins_;
+    delete[] bins_;
+  }
+  uint8_t dimensions_;
+  Binning *binnings_;
+  uint32_t *max_bins_;
 
-  class BinningDynamic : public Binning<BinningDynamic> {
-    friend class Binning<BinningDynamic>;
-   public:
-    // TODO: move parameter
-    BinningDynamic(const std::vector<Floating> &borders)
-      : borders_(borders)
-      , max_bin_(borders_.size())
-    { }
-
-   protected:
-    const uint8_t kSteps = 16;
-    uint32_t FindBinImpl(Floating val) const {
-      if (val < borders_[0]) return 0;
-      if (val >= borders_[max_bin_-1]) return max_bin_;
-
-      uint32_t lo = 0;
-      uint32_t hi = max_bin_;
-      while (hi-lo > kSteps) {
-        uint32_t skip = (hi-lo)/kSteps;
-        unsigned i = lo + skip;
-        for ( ; i < hi; i += skip)
-          if (borders_[i] >= val) break;
-        hi = (i > hi) ? hi : i;
-        lo = i - skip;
-      }
-      unsigned i = lo;
-      for ( ; i < hi; ++i)
-        if (borders_[i] > val) break;
-      return i;
-    }
-    uint32_t GetMax() const { return max_bin_; }
-    std::vector<Floating> borders_;
-    const uint32_t max_bin_;
-  };
-
-
-  Histogram() { }
-
+  ChannelType *bins_;
 };
 
 #endif
