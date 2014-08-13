f585470a0457c47a59a9e1d7f466006361bdcd3f
Jakob Blomer <jblomer@cern.ch>
2014-08-13 14:25:54 -0700

    one large table, variable number of worker nodes

--- merger/merger.cc
+++ merger/merger.cc
@@ -13,17 +13,28 @@
 #include "md5.h"
 
 const unsigned kMaxHName = 1024;
-const unsigned kNumNodes = 1;
-const unsigned kBatchSize = 1000;
+const unsigned kBatchSize = 10000;
 
 struct rc_client *client;
+uint64_t global_tblid;
 
-void FlushBuffer(std::vector<uint64_t> *tblids,
+struct JointKey {
+  JointKey(const uint32_t logical_tblid, const uint64_t key)
+    : logical_tblid(logical_tblid)
+    , key(key)
+  {}
+  uint32_t logical_tblid;
+  uint64_t key;
+} __attribute__((packed));
+
+void FlushBuffer(std::vector<uint32_t> *logical_tblids,
                  std::vector<int64_t> *keys, 
-                 std::vector<double> *values)
+                 std::vector<double> *values,
+                 const bool dry_run)
 {
-  assert((tblids->size() == keys->size()) && (keys->size() == values->size()));
-  const unsigned N = tblids->size();
+  assert((logical_tblids->size() == keys->size()) && 
+         (keys->size() == values->size()));
+  const unsigned N = logical_tblids->size();
   if (N == 0) return;
   
   Prng prng;
@@ -42,57 +53,96 @@ void FlushBuffer(std::vector<uint64_t> *tblids,
   }
 
   printf("INCREMENTING %u BINS\n", N);
+  std::vector<JointKey> joint_keys;
+  joint_keys.reserve(N);
+  for (unsigned i = 0; i < N; ++i) {
+    joint_keys.push_back(JointKey((*logical_tblids)[i], (*keys)[i]));
+  }
+  
   const uint16_t szMultiOpIncrement = rc_multiOpSizeOf(MULTI_OP_INCREMENT);
   unsigned char *mIncrementObjects = reinterpret_cast<unsigned char *>
       (malloc(N * szMultiOpIncrement));
   void **pmIncrementObjects = reinterpret_cast<void **>
       (malloc(N * sizeof(pmIncrementObjects[0])));
+  
   for (unsigned i = 0; i < N; ++i) {
       pmIncrementObjects[i] = mIncrementObjects + (i * szMultiOpIncrement);
-      rc_multiIncrementCreate(tblids->at(shuffled[i]), 
-                              &keys->at(shuffled[i]), sizeof(int64_t),
-                              0, values->at(shuffled[i]), 
+      rc_multiIncrementCreate(global_tblid, 
+                              &joint_keys[shuffled[i]], sizeof(JointKey),
+                              0, (*values)[shuffled[i]], 
                               NULL, pmIncrementObjects[i]);
   }
-  rc_multiIncrement(client, pmIncrementObjects, N);
+  if (!dry_run) {
+    rc_multiIncrement(client, pmIncrementObjects, N);
   
-  free(shuffled);
-  
-  for (unsigned i = 0; i < N; ++i) {
+    for (unsigned i = 0; i < N; ++i) {
       Status thisStatus = 
-          rc_multiOpStatus(pmIncrementObjects[i], MULTI_OP_INCREMENT);
+        rc_multiOpStatus(pmIncrementObjects[i], MULTI_OP_INCREMENT);
       if (thisStatus != STATUS_OK) {
         printf("upload failure at %u\n", i);
         abort();
       }
       rc_multiOpDestroy(pmIncrementObjects[i], MULTI_OP_INCREMENT);
+    }
   }
+  free(shuffled);
   free(pmIncrementObjects);
   free(mIncrementObjects);
   
-  tblids->clear();
+  logical_tblids->clear();
   keys->clear();
   values->clear();
 }
 
+void Usage(const char *progname) {
+    printf("Usage: %s <input file> <table name> <num of workers> [locator]\n",
+           progname);
+}
+
 int main(int argc, char **argv) {
-  Status status;
-  const char *locator = "infrc:host=192.168.1.119,port=11100";
-  if (argc >= 3)
-    locator = argv[2];
-  status = rc_connect(locator, "main", &client);
-  if (status != STATUS_OK) {
-    printf("failed connecting to RAMCloud\n");
-    abort();
+  if (argc < 4) {
+    Usage(argv[0]);
+    return 1;
   }
   
   char mode = argv[1][0];
   char *input = &(argv[1][1]);
-  printf("Using file %s\n", input);
+  printf("Using file %s, mode %c\n", input, mode);
   int fd_input = open(input, O_RDONLY);
-
+  
+  if (mode != 'D') {
+    Status status;
+    const char *locator = "infrc:host=192.168.1.119,port=11100";
+    if (argc >= 5)
+      locator = argv[4];
+    status = rc_connect(locator, "main", &client);
+    if (status != STATUS_OK) {
+      printf("failed connecting to RAMCloud\n");
+      abort();
+    }
+  
+    const char *table_name = argv[2];
+    const uint32_t num_nodes = atoi(argv[3]);
+    status = rc_getTableId(client, table_name, &global_tblid);
+    if (status != STATUS_TABLE_DOESNT_EXIST) {
+      printf("table %s exists, stop\n", table_name);
+      abort();
+    }
+    status = rc_createTable(client, table_name, num_nodes);
+    if (status != STATUS_OK) {
+      printf("failed creating table %s\n", table_name);
+      abort();
+    }
+    status = rc_getTableId(client, table_name, &global_tblid);
+    if (status != STATUS_OK) {
+      printf("failed getting global table id\n");
+      abort();
+    }
+  }
+  
   unsigned ntables = 0;
-  std::vector<uint64_t> tblids;
+  unsigned totalBins = 0;
+  std::vector<uint32_t> logical_tblids;
   std::vector<int64_t> keys;
   std::vector<double> values;
   while (1) {
@@ -130,22 +180,10 @@ int main(int argc, char **argv) {
     }
     table_name[32] = '\0';
     
-    if (mode == 'C') {
-      printf("Creating table %s for histogram %s\n", table_name, name_buf);
-      status = rc_createTable(client, table_name, kNumNodes);
-      if (status != STATUS_OK) {
-        printf("failed creating table\n");
-        abort();
-      }
-    }
-    uint64_t tblid;
     if (mode == 'I') {
-      status = rc_getTableId(client, table_name, &tblid);
-      if (status != STATUS_OK) {
-        printf("failed to get table id\n");
-        abort();
-      }
-      printf("Table id for table name %s is %lu (%s)\n", table_name, tblid ,name_buf);
+      printf("Merging histogram %s (%s)\n", table_name, name_buf);
+    } else if (mode == 'D') {
+      printf("Dry-run merging histogram %s (%s)\n", table_name, name_buf);
     }
     
     unsigned nbins = 0;
@@ -158,19 +196,25 @@ int main(int argc, char **argv) {
       if (item.bin == -1)
         break;
       
-      if (mode == 'I') {
-        tblids.push_back(tblid);
+      if ((mode == 'I') || (mode == 'D')) {
+        logical_tblids.push_back(ntables);
         keys.push_back(item.bin);
         values.push_back(item.value);
       }
       nbins++;
+      totalBins++;
       
-      if (keys.size() == kBatchSize) FlushBuffer(&tblids, &keys, &values);
+      if (keys.size() == kBatchSize) {
+        FlushBuffer(&logical_tblids, &keys, &values, mode == 'D');
+      }
     }
     printf("... %u bins\n", nbins);
+    //if (mode == 'E') {
+    //  
+    //}
   }
-  FlushBuffer(&tblids, &keys, &values);
+  FlushBuffer(&logical_tblids, &keys, &values, mode == 'D');
   
-  printf("finished reading (%u tables)\n", ntables);
+  printf("finished reading (%u tables, %u bins)\n", ntables, totalBins);
   return 0;
 }
